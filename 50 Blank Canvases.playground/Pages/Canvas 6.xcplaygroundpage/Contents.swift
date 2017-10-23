e9982e863ec4b49e2f651c147df2044f45beb72f
Zang Eric <ezang@lcs-mba3154.lcs.on.ca>
2017-10-23 17:21:06 -0400

    Used Perlin Noise Generator to optimize hue colour

--- 50 Blank Canvases.playground/Pages/Canvas 6.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 6.xcplaygroundpage/Contents.swift
@@ -13,13 +13,16 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 400, height: 400)
 
 // Create a starting point for travelling through Perlin noise space
 var start = 0.0
 
 //Create a Perlin noise generator
 let p = PerlinGenerator()
+
+// for saving prior colour
+var lastColour = 0.0
 
 
 for i in stride(from: 50, through: 600, by: 100){
@@ -30,43 +33,33 @@ for i in stride(from: 50, through: 600, by: 100){
             
             // Color generator
 
-           
+           //Hue
             //Move through Perlin noise space
-            start += 5
+            start += 0.2
             
-            //Get Perlin noise (gives us a random number between -3 and 3)
-            let randomValue = g.perlinNoise(x: start)
+            //Get Perlin noise (gives us a random number between -1 and 1)
+            let value = p.perlinNoise(x: start)
             
             //Convert to a range between 0 and 100
-            let height = map(value: randomValue, fromLower: -3, fromUpper: 3, toLower: 1, toUpper: 361)
+            var newColour = map(value: value, fromLower: -1, fromUpper: 1, toLower: 1, toUpper: 360)
+
+            //Test if number are too close
+            if newColour - lastColour > -30 && newColour - lastColour < 30 {
+                newColour += 100
+            }
             
-            // Make the height an integer
-            let g = Int(height)
-            
+            //Random saturation and brightness
             let s = random(from: 50, toButNotIncluding: 101)
-            
-            
-            //Brightness
-            //Move through Perlin noise space
-            start += 5
-            
-            //Get Perlin noise (gives us a random number between -3 and 3)
-            let randomValue = p.perlinNoise(x: start)
-            
-            //Convert to a range between 0 and 100
-            let height = map(value: randomValue, fromLower: -3, fromUpper: 3, toLower: 50, toUpper: 100)
-            
-            // Make the height an integer
-            let toY = Int(height)
-            
-            
+            let b = random(from: 75, toButNotIncluding: 101)
             
             // Color
-            canvas.fillColor = Color.init(hue: g, saturation: s, brightness: toY, alpha: 100)
+            canvas.fillColor = Color.init(hue: Int(newColour), saturation: s, brightness: b, alpha: 100)
             // Drawing the circle
             canvas.drawEllipse(centreX: i, centreY: y, width: l, height: l)
             
-            
+            // Save prior colour
+            lastColour = newColour
+
         }
     }
 }
